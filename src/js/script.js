c7311e85bc6dbdfa71fb42cbd21bc3db23c2d901
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-04-28 22:45:18 +0200

    Add modals

--- src/js/script.js
+++ src/js/script.js
@@ -45,10 +45,49 @@ var chart = new Chart (ctx, { // eslint-disable-line
 /* Menu */
 
 function toggleMenu(visible) {
-  document.querySelector('.menu').classList.toggle('show', visible)
+  document.querySelector('.menu').classList.toggle('show', visible);
 }
 
 document.querySelector('.hamburger').addEventListener('click', function(e) {
   e.preventDefault();
-  toggleMenu()
+  toggleMenu();
+});
+
+/* Modals */
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+document.querySelector('.popup').addEventListener('click', function(e) {
+  if(e.target === this) {
+    openModal('#modalManager');
+  }
 });
